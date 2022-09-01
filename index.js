b68cb265266117a0696680bdd48683163f6069ca
Alexis Torosina <alexistorosina1@gmail.com>
2022-08-31 20:41:27 -0400

    commit tree f417d1b2cd0f82a9e9432b6a08d0922d71963a0c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+    const cart = []
+    for (let i = 0; i < names.length; i++) {
+        cart.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cart;
+}
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
